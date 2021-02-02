1eeacd22c262b49686268a93ed625c3032782c72
msm79 <mohammadmarandi89@gmail.com>
2021-02-03 01:04:09 +0330

    create AddTodo Component

---
+++ src/Component/AddTodo/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import './style.css';
+
+import { Drawer, Form, Button, Col, Row, Input, DatePicker } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+class DrawerForm extends React.Component {
+  state = { visible: false };
+
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+      <Row>
+        <Button type="primary" onClick={this.showDrawer} danger>
+          <PlusOutlined /> New Task
+        </Button>
+      </Row>
+        <Drawer
+          title="New Task"
+          width={720}
+          onClose={this.onClose}
+          visible={this.state.visible}
+          bodyStyle={{ paddingBottom: 80 }}
+          footer={
+            <div
+              style={{
+                textAlign: 'right',
+              }}
+            >
+              <Button onClick={this.onClose} type="primary">
+                Submit
+              </Button>
+            </div>
+          }
+        >
+          <Form layout="vertical" hideRequiredMark>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item
+                  name="name"
+                  label="Title"
+                  rules={[{ required: true, message: 'Please enter user name' }]}
+                >
+                  <Input placeholder="Title" />
+                </Form.Item>
+              </Col>
+
+              <Col span={12}>
+                <Form.Item
+                  name="dateTime"
+                  label="DateTime"
+                  rules={[{ required: true, message: 'Please choose the dateTime' }]}
+                >
+                  <DatePicker
+                    style={{ width: '100%' }}
+                    getPopupContainer={trigger => trigger.parentElement}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item
+                  name="description"
+                  label="Description"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'please enter url description',
+                    },
+                  ]}
+                >
+                  <Input.TextArea rows={4} placeholder="please enter url description" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </Drawer>
+      </>
+    );
+  }
+}
+
+export default DrawerForm;
