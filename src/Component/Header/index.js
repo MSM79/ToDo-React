47d29d4b2a6b336329056439c4e4a27ef348a971
msm79 <mohammadmarandi89@gmail.com>
2021-02-05 04:06:08 +0330

    Fix header styles

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import { PageHeader, Avatar } from 'antd';
-import './style.css';
+import { Row, Col } from 'antd';
 import AddTodo from '../AddTodo';
+import './style.css';
+
 
 function Header() {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  const weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+  const date = new Date();
+
   return (
-    <header>
-      <PageHeader
-      title="ToDo"
-      className="site-page-header"
-      />
+    <Row justify="space-around" align="middle" id="headerow">
+      <Col>
+        <h1>ToDo</h1>
+      </Col>
 
-      <AddTodo />
+      <Col>
+        <AddTodo />
+      </Col>
 
-      <PageHeader
-      className="avatar"
-      title="Mohammad Marandi"
-      avatar={
-        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-      }/>
-    </header>
+      <Col>
+        <p class="time">{ weekday[date.getDay()] + ',' +monthNames[date.getMonth()] + ' ' + date.getDay()}</p>
+      </Col>
+    </Row>
   )
 }
 
