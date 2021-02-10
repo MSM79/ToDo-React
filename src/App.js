1f9ce45fa1e4564fd6e0ec73862fc19b43a7f1e8
msm79 <mohammadmarandi89@gmail.com>
2021-02-10 14:07:35 +0330

    Del components & add layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
 import React from 'react';
-import Todo from './Component/Todo';
-import Header from './Component/Header';
 import './App.css';
+import MyLayout from './Component/Layout';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div class="todo">
-        <Todo
-          title="Mesvak"
-          description="Bayad Mesvak Bezanam"
-          time="10:15"
-         />
-        <Todo
-        title="Game"
-        description="blueblueblue"
-        time="11:00"
-        />
-        <Todo
-          title="Coding"
-          description="Todoye Binamoos"
-          time="7:07"
-        />
-      </div>
-    </div>
+      <MyLayout />
   )
 }
 
